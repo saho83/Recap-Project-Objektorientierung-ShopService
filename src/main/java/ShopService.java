042193398aa634fd5862d1e21a97d943e486078b
sascha horn <saschahorn83@gmail.com>
2023-11-20 16:19:45 +0100

    add getOrdersByStatus()

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
@@ -20,5 +21,11 @@ public class ShopService {
         Order newOrder = new Order(UUID.randomUUID().toString(), products);
 
         return orderRepo.addOrder(newOrder);
+    }
+
+    public List<Order> getOrdersByStatus(OrderStatus status) {
+        return orderRepo.getOrders().stream()
+                .filter(order -> order.status() == status)
+                .collect(Collectors.toList());
     }
 }
